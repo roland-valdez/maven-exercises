a0d25202897ec24769f4042648d208a12e9d161a
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-13 14:03:24 -0500

    completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,22 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("StringUtils.isAllUpperCase(\"Hello\") = " + StringUtils.isAllUpperCase("Hello"));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type in something to analyze it");
+        String input = scanner.nextLine();
+        if(StringUtils.isNumeric(input)){
+            System.out.println("What you entered is a number.");
+        }else {
+            System.out.println("What you entered is not a number");
+            System.out.println("This is your input with the cases flipped");            ;
+            System.out.println(StringUtils.swapCase(input));
+            System.out.println("This is your input reversed character by character:");
+            System.out.println(StringUtils.reverse(input));
+        }
+
+
     }
 }
